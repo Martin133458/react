fa9c70962077dd46ec6bc268db55f208d8288719
Martin133458 <perdunstaryj76@gmail.com>
2024-10-22 13:06:22 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="header">
+
+        <img src="https://i.pinimg.com/736x/8c/8e/66/8c8e66f77c287e1748eadb7f7ce66f8f.jpg" alt="" className="logo" />
+        <div className="menu">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Comment</a>
+        </div>
+        <img src="/pngwing.com.png" alt="%" className='img' />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
